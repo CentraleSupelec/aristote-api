d3903f273d10b62d94d439aec9733f901110a71d
Mohamed Ali BARKA <mohamed-ali.barka@illuin.tech>
2023-10-06 12:59:15 +0200

    Added error handling

--- src/Constants.php
+++ src/Constants.php
@@ -7,8 +7,8 @@ use League\Bundle\OAuth2ServerBundle\OAuth2Grants;
 final class Constants
 {
     public const ROLE_SUPER_ADMIN = 'ROLE_SUPER_ADMIN';
-
     public const SCOPE_DEFAULT = 'scope_default';
+    public const SORT_ORDER_OPTIONS = ['asc', 'desc'];
 
     public static function getAvailableScopes(): array
     {
--- src/Utils/PaginationUtils.php
+++ src/Utils/PaginationUtils.php
@@ -2,6 +2,7 @@
 
 namespace App\Utils;
 
+use App\Constants;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 
 class PaginationUtils
@@ -21,5 +22,27 @@ class PaginationUtils
         $lastPage = (int) ceil($totalItemCount / $size);
 
         return $page === $lastPage;
+    }
+
+    public function paginationRequestParametersValidator(array $possibleSortFields, string $sort, string $order, int $size, int $page): array
+    {
+        $errors = [];
+        if (!in_array($sort, $possibleSortFields)) {
+            $errors[] = sprintf("Sort field '%s' is not supported", $sort);
+        }
+
+        if (!in_array(strtolower($order), Constants::SORT_ORDER_OPTIONS)) {
+            $errors[] = sprintf("Sort order '%s' is not valid, valid values are : 'DESC' or 'ASC'", $order);
+        }
+
+        if ($size < 1) {
+            $errors[] = sprintf("Size '%s' is not valid, it should be an integer >= 1", $size);
+        }
+
+        if ($page < 1) {
+            $errors[] = sprintf("Page '%s' is not valid, it should be an integer >= 1", $page);
+        }
+
+        return $errors;
     }
 }
